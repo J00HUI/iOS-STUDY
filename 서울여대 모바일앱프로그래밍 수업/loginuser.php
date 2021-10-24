6c83b5d471b0a2eb7d9a8d4f63e19eb7b4c96f3c
LeeJuHee <83942393+ERIN56@users.noreply.github.com>
2021-10-24 18:55:33 +0900

    Update loginuser.php

--- 서울여대 모바일앱프로그래밍 수업/loginuser.php
+++ 서울여대 모바일앱프로그래밍 수업/loginuser.php
@@ -1,21 +1,26 @@
 <?php
     $conn = mysqli_connect('localhost', 'root', 'root', 'iphone');
-
-    header('Content-Type: charset=utf-8');
-
     $userid = ($_POST['id']);
-    $password = ($_POST['password']);
-    $password = md5($password);
-    $name = ($_POST['name']);
+    $sql = "select userid, passwd, name from user where userid = '$userid' limit 1";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_row($result)) {
+            $dbUserid = $row[0];
+            $dbPassword = $row[1];
+            $dbName = $row[2];
 
-    $sql = "insert into user (userid, passwd, name) values ('$userid', '$password', '$name')";
+            $password = ($_POST['password']);
+            $password = md5($password);
 
-    if(mysqli_query($conn, $sql)){
-        echo 'insert Done!!!';
+            if($userid == $dbUserid && $password == $dbPassword) {
+                echo json_encode(array("success"=>"YES", "name"=>$dbName));
+            } else {
+                echo json_encode(array("success"=>"NO", "error"=>"Password is invalid"));
+            }
+        }
     } else {
-        echo mysqli_error($conn);
+        echo json_encode(array("success"=>"NO", "error"=>"ID not exist"));
     }
 
     mysqli_close($conn);
-
 ?>
