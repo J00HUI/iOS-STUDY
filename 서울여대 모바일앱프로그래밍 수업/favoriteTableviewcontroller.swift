c54fb8cadfc9c78669da8b17a409549ed11d8407
LeeJuHee <83942393+ERIN56@users.noreply.github.com>
2021-10-24 18:49:33 +0900

    Create favoriteTableviewcontroller.swift

---
+++ 서울여대 모바일앱프로그래밍 수업/favoriteTableviewcontroller.swift
@@ -0,0 +1,153 @@
+//  FavoriteTableViewController.swift
+//  ServerLogin
+//
+//  Created by 이주희 on 2021/10/11.
+
+
+import UIKit
+
+class FavoriteTableViewController: UITableViewController {
+
+    var fetchedArray: [FavoriteData] = Array()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        if let name = appDelegate.userName {
+            self.title = name + "'s Favorite"
+        }
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        fetchedArray = [] // 배열을 초기화하고 서버에서 자료를 다시 가져옴
+        self.downloadDataFromServer()
+    }
+    
+    func downloadDataFromServer() -> Void {
+        let urlString: String = "http://localhost:8888/favorite/favoriteTable.php"
+        guard let requestURL = URL(string: urlString) else {
+            return
+        }
+        let request = URLRequest(url: requestURL)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) {
+            (responseData, response, responseError) in
+            guard responseError == nil else {
+                print("Error: calling POST")
+                return
+            }
+            guard let receivedData = responseData else {
+                print("Error: not receiving Data")
+                return
+            }
+            let response = response as! HTTPURLResponse
+            if !(200...299 ~= response.statusCode) {
+                print("HTTP resonse Error!")
+                return
+            }
+            do {
+                if let jsonData = try JSONSerialization.jsonObject(with: receivedData, options: .allowFragments) as? [[String:Any]] {
+                    
+                    for i in 0...jsonData.count-1 {
+                        let newData: FavoriteData = FavoriteData()
+                        let jsonElement = jsonData[i]
+                        
+                        newData.favoriteno = jsonElement["favoriteno"] as! String
+                        newData.userid = jsonElement["id"] as! String
+                        newData.name = jsonElement["name"] as! String
+                        newData.descript = jsonElement["descript"] as! String
+                        newData.imageName = jsonElement["imageName"] as! String
+                        newData.date = jsonElement["date"] as! String
+                        
+                        self.fetchedArray.append(newData)
+                    }
+                    
+                    DispatchQueue.main.sync {
+                        self.tableView.reloadData()
+                    }
+                }
+                
+            }
+            catch {
+                print("Error: Catch")
+            }
+        }
+        task.resume()
+    }
+    
+    @IBAction func buttonLogout(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "로그아웃 하시겠습니까?", message: "", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: {
+            action in
+            let urlString : String = "http://localhost:8888/login/logout.php"
+            guard let requestURL = URL(string: urlString) else {
+                return
+            }
+            
+            var request = URLRequest(url: requestURL)
+            request.httpMethod = "POST"
+            let session = URLSession.shared
+            let task = session.dataTask(with: request) {
+                (responseData, response, responseError) in
+                guard responseError == nil else {
+                    return
+                }
+            }
+            task.resume()
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let loginView = storyboard.instantiateViewController(identifier: "LoginView")
+            self.present(loginView, animated: true, completion: nil)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+        self.present(alert, animated: true)
+    }
+    
+
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return fetchedArray.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Favorite Cell", for: indexPath)
+        
+        let item = fetchedArray[indexPath.row]
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = item.date
+
+        return cell
+    }
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "toDetailView"{
+            if let destination = segue.destination as? DetailViewController {
+                if let selectedIndex = self.tableView.indexPathsForSelectedRows?.first?.row {
+                    let data = fetchedArray[selectedIndex]
+                    destination.selectedData = data
+                    destination.title = data.name
+                }
+                
+            }
+        }
+    }
+
+}
