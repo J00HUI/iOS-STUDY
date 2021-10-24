8049f35a31ee0ef241edf8a8266301dfcbaaf891
LeeJuHee <83942393+ERIN56@users.noreply.github.com>
2021-10-24 18:52:15 +0900

    Create favoritedata.swift

---
+++ 서울여대 모바일앱프로그래밍 수업/favoritedata.swift
@@ -0,0 +1,16 @@
+//  FavoriteData.swift
+//  ServerLogin
+//
+//  Created by 이주희 on 2021/10/24.
+
+import UIKit
+
+class FavoriteData: NSObject {
+    var favoriteno: String = ""
+    var userid: String = ""
+    var name: String = ""
+    var descript: String = ""
+    var imageName: String = ""
+    var date: String = ""
+
+}
